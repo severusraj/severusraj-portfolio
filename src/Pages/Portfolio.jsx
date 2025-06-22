7f6ceec58b130d2eef6d3299ec64e59e589a37d9
RA <rangelojam@gmail.com>
2025-06-23 07:03:23 +0800

    Refactor dependency for swipeable views in package.json and Portfolio.jsx

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState, useCallback } from "react";
 import { supabase } from "../supabase"; 
 
 import PropTypes from "prop-types";
-import SwipeableViews from "react-swipeable-views-react-18-fix";
+import SwipeableViews from "react-swipeable-views";
 import { useTheme } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Tabs from "@mui/material/Tabs";
